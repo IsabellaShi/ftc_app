d9ac21df9b90e87dff13cd4d90c87ee7b6705c91
shiyaoran <syr_an@126.com>
2017-09-04 16:54:39 -0700

    Hello

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+/**
+ * Created by shiyaoran on 2017/9/4.
+ */
+
+public class Test extends OpMode{
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
