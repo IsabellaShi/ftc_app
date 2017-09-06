5e5f01659702404f54446ead97f0af879825ae36
shiyaoran <syr_an@126.com>
2017-09-06 15:43:37 -0700

    Hello

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SecondMotorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SecondMotorTest.java
@@ -11,19 +11,16 @@ import org.firstinspires.ftc.teamcode.modules.GamepadV2;
  */
 
 public class SecondMotorTest extends OpMode{
-    private DcMotor frontLeft, backLeft, frontRight, backRight;
+    private DcMotor Left, Right;
 
     private GamepadV2 pad1 = new GamepadV2();
 
     @Override
     public void init() {
-        frontLeft = hardwareMap.dcMotor.get("FL");
-        backLeft = hardwareMap.dcMotor.get("BL");
-        frontRight = hardwareMap.dcMotor.get("FR");
-        backRight = hardwareMap.dcMotor.get("BR");
+        Left = hardwareMap.dcMotor.get("L");
+        Right = hardwareMap.dcMotor.get("R");
 
-        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
-        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        Left.setDirection(DcMotorSimple.Direction.REVERSE);
 
     }
 
@@ -31,10 +28,7 @@ public class SecondMotorTest extends OpMode{
     public void loop() {
         pad1.update(gamepad1);
 
-        frontLeft.setPower(pad1.left_stick_y_exponential(1));
-        backLeft.setPower(pad1.left_stick_y_exponential(1));
-        frontRight.setPower(pad1.right_stick_y_exponential(1));
-        backRight.setPower(pad1.right_stick_y_exponential(1));
-
+        Left.setPower(pad1.left_stick_y_exponential(1));
+        Right.setPower(pad1.right_stick_y_exponential(1));
     }
 }
